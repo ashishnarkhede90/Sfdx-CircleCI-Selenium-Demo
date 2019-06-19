ce3ec796b1c8b0a4033b632bde6c2a8b1779c9d9
Ashish Narkhede <ashish.narkhede90@gmail.com>
2019-06-19 16:11:29 +0530

    Reverting - u know what

--- build/setup_virtualframebuffer.sh
+++ build/setup_virtualframebuffer.sh
@@ -1,16 +1,4 @@
 #!/bin/zsh
 echo "Running X virtual framebuffer"
 Xvfb :0 -ac &
-export DISPLAY=:99 
-
-un=$USERNAME
-pw=$PASSWORD
-mkdir data
-cat > data/credentials.json <<EOF
-	{
-	 "username": $un,
-	 "password": $pw
-	}
-EOF
-
-cat data/credentials.json
+export DISPLAY=:99
